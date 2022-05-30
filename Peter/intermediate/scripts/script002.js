6294601d18fc172de75f8addd3e10c376edfa305
Peter Aigner <mastapetz@icloud.com>
2022-05-30 11:22:13 +0200

    finish intermediate

--- Peter/intermediate/scripts/script002.js
+++ Peter/intermediate/scripts/script002.js
@@ -2,3 +2,22 @@
   console.log("JS Connected");
 })();
 // ---------------------------------------------------------
+// Capitalize
+const string = function (str) {
+  // let str = "i am a programmer";
+  // let str2 = str.charAt(0);
+  let str1 = str.charAt(0).toUpperCase() + str.slice(1);
+  return console.log(str1);
+};
+string("i am a programmer");
+// ---------------------------------------------------------
+// Average grade
+const averageGrade = (maths, physics, english) => {
+  let mathGrade = Number(maths);
+  let physicsGrade = Number(physics);
+  let englishGrade = Number(english);
+  console.log(`Sum: ${mathGrade + physicsGrade + englishGrade}`);
+  console.log(`Average: ${(mathGrade + physicsGrade + englishGrade) / 3}`);
+  return;
+};
+averageGrade(4, 4, 4);
