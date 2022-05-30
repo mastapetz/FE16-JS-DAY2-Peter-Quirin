9f82deb8fc0eba3927f2e6575f3246576f0cd7f8
Peter Aigner <mastapetz@icloud.com>
2022-05-30 15:31:24 +0200

    new version challenge

--- Peter/challenge/scripts/script003.js
+++ Peter/challenge/scripts/script003.js
@@ -3,80 +3,87 @@
 })();
 // ------------------ATM---------------------------------------
 // ATM Stock at start
-var note_Hundred = 2;
-var note_fifty = 1;
-var note_twenty = 1;
-var note_ten = 1;
+var note_hundred = 120;
+var note_fifty = 200;
+var note_twenty = 100;
+var note_ten = 500;
+var total_amount =
+  note_hundred * 100 + note_fifty * 50 + note_twenty * 20 + note_ten * 10;
+// var hundred_notes = 0;
+// var hundred_Fit = 0;
+// var fifty_notes = 0;
+// var fifty_fit = 0;
+// var twenty_notes = 0;
+// var twenty_fit = 0;
+// var ten_notes = 0;
+// var ten_fit = 0;
 // ------------------
 const atm = (moneyget) => {
-  // check if the entered money is over the current stock
+  // ================================Check stock
   if (
     moneyget >
-    note_Hundred * 100 + note_fifty * 50 + note_twenty * 20 + note_ten * 10
+    note_hundred * 100 + note_fifty * 50 + note_twenty * 20 + note_ten * 10
   ) {
     return console.log("Not enough notes available");
   }
-  // if enough money calculate notes
-  else {
-    // check if 100s are needed
-    if (Math.trunc(moneyget / 100) === 0 || moneyget % 10 >= 1) {
-      // if no hundreds are needed check lower notes
-      if (Math.trunc(moneyget / 10) === 1) {
-        note_ten -= 1;
-        var note_ten2 = 1;
-      } else if (Math.trunc(moneyget / 10) === 2) {
-        note_twenty -= 1;
-        var note_twenty2 = 1;
-      } else if (Math.trunc(moneyget / 10) === 3) {
-        note_ten -= 1;
-        note_twenty -= 1;
-        var note_ten2 = 1;
-        var note_twenty2 = 1;
-      } else if (Math.trunc(moneyget / 10) === 5) {
-        note_fifty -= 1;
-        var note_fifty2 = 1;
-      } else if (Math.trunc(moneyget / 10) === 6) {
-        note_ten -= 1;
-        note_fifty -= 1;
-        var note_ten2 = 1;
-        var note_fifty2 = 1;
-      } else if (Math.trunc(moneyget / 10) === 7) {
-        note_twenty -= 1;
-        note_fifty -= 1;
-        var note_twenty2 = 1;
-        var note_fifty2 = 1;
-      } else if (Math.trunc(moneyget / 10) === 8) {
-        note_ten -= 1;
-        note_twenty -= 1;
-        note_fifty -= 1;
-        var note_ten2 = 1;
-        var note_twenty2 = 1;
-        var note_fifty2 = 1;
-      }
-
-      // Error messages if amount is not covered by notes or below 10
-      else if (Math.trunc(moneyget / 10) === 0) {
-        return console.log(
-          "Only Notes of Hundred, Fifty, Twenty and Ten available"
-        );
-      } else {
-        console.log("Not enough notes in combination of 10 20 50 available");
-      }
-    }
-
-    // calculate if enough 100s are in the machine and lower stuck for given amount
-    else if (Math.trunc(moneyget / 100) > note_Hundred) {
-      return console.log("Not enough 100 € notes available");
-    } else if (Math.trunc(moneyget / 100) === 1) {
-      note_Hundred -= 1;
-      var note_Hundred2 = 1;
-    } else if (Math.trunc(moneyget / 100) === 2) {
-      note_Hundred -= 2;
-      var note_Hundred2 = 2;
-    }
+  // ================================
+  let hundred_notes = 0;
+  hundred_Fit = Math.trunc(moneyget / 100); // How many 100s fit in the amount
+  console.log("Einzelne Hunderter = " + hundred_Fit);
+  if (hundred_Fit > note_hundred) {
+    // If there are not enough 100 bills in account, take out all
+    hundred_notes = note_hundred;
+  } else {
+    hundred_notes = hundred_Fit; // Else take out how many bills fit into the amount
   }
+  moneyget = moneyget - hundred_notes * 100;
+  console.log("Moneyget nach 100er = " + moneyget);
+  // ================================
+  let fifty_notes = 0;
+  fifty_fit = Math.trunc(moneyget / 50); // How many 50s fit in the amount
+  console.log("Fufziger = " + fifty_fit);
+  if (fifty_fit > note_fifty) {
+    // If there are not enough 50 bills in account, take out all
+    fifty_notes = note_fifty;
+  } else {
+    fifty_notes = fifty_fit; // Else take out how many bills fit into the amount
+    console.log("Einzelne fufziga " + fifty_notes);
+  }
+  moneyget = moneyget - fifty_notes * 50;
+  console.log("Nach Fufziger = " + moneyget);
+  //   ================================
+  let twenty_notes = 0;
+  twenty_fit = Math.trunc(moneyget / 20); // How many 50s fit in the amount
+  console.log("zwanziger = " + twenty_fit);
+  if (twenty_fit > note_twenty) {
+    // If there are not enough 50 bills in account, take out all
+    twenty_notes = note_twenty;
+  } else {
+    twenty_notes = twenty_fit; // Else take out how many bills fit into the amount
+    console.log("Einzelne zwanziga " + twenty_notes);
+  }
+  moneyget = moneyget - twenty_notes * 20;
+  console.log("Nach zwanziger = " + moneyget);
+
+  // ================================
+  let ten_notes = 0;
+  ten_fit = Math.trunc(moneyget / 20); // How many 50s fit in the amount
+  console.log("zehner = " + ten_fit);
+  if (ten_fit > note_ten) {
+    // If there are not enough 50 bills in account, take out all
+    ten_notes = note_ten;
+  } else {
+    ten_notes = ten_fit; // Else take out how many bills fit into the amount
+    console.log("Einzelne zehna " + ten_notes);
+  }
+  moneyget = moneyget - ten_notes * 20;
+  console.log("Nach zehner = " + moneyget);
+
+  // ================================
   return console.log(
-    `You get ${note_Hundred2} in 100 notes ${note_fifty2} in 50 notes ${note_twenty2} in 20 notes and ${note_ten2} in 10 notes.`
+    `You get ${hundred_notes} in 100 notes ${fifty_notes} in 50 notes ${twenty_notes} in 20 notes ${ten_notes} in 10 notes`
   );
+  console.log(moneyget);
 };
-atm(110);
+console.log("Total Amount = " + total_amount);
+atm(380);
